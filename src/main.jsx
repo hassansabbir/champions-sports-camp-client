d447c199945cdfc6b1ec760719b33ed0152545ac
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-06-08 15:49:42 +0600

    authPrivider added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
+import AuthProvider from "./Providers/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div className="max-w-7xl mx-auto">
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  </div>
+  <React.StrictMode>
+    <AuthProvider>
+      <div className="max-w-7xl mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
+  </React.StrictMode>
 );
