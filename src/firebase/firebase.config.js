eeba708acb0818f671fce05be6fbbd57bdd4bb58
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-06-08 15:10:48 +0600

    signup page added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBExMkNy3MSeoah6-KLxhx0MHMti0JBU0Y",
+  authDomain: "champions-sports-camp.firebaseapp.com",
+  projectId: "champions-sports-camp",
+  storageBucket: "champions-sports-camp.appspot.com",
+  messagingSenderId: "719737268952",
+  appId: "1:719737268952:web:59c5354f60d53a852fc5ad",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
