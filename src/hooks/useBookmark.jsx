81b17d318ca42bf0fa50ea8f65c22c9a7dd11da9
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-06-09 16:14:01 +0600

    tanstack query added

--- src/hooks/useBookmark.jsx
+++ src/hooks/useBookmark.jsx
@@ -1,2 +1,23 @@
-const useBookmark = (email) => {};
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+
+const useBookmark = () => {
+  const { user } = useContext(AuthContext);
+
+  const {
+    isLoading,
+    data: bookmark = [],
+    refetch,
+  } = useQuery({
+    queryKey: ["bookmarks", user?.email],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:5000/bookmarks?email=${user?.email}`
+      );
+      return response.json();
+    },
+  });
+  return [bookmark, isLoading, refetch];
+};
 export default useBookmark;
